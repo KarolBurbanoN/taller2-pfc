32d799d35c2e487385a376e7e011fa0bfa160f9e
Sebastian C <134980272+Sebas7w7@users.noreply.github.com>
2024-11-07 22:34:24 -0500

    Se añade la función complemento y inclusión
    se agregan al código la funcion de complemento y la funcion de inclusion

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -22,4 +22,26 @@ class ConjuntosDifusos {
     evaluarGrande
   }
 
+  def complemento(c: ConjDifuso): ConjDifuso = {
+    // Creamos una función que calcula el complemento
+    def calcularComplemento(x: Int): Double = {
+      // El complemento es 1 - f(x)
+      1.0 - c(x)
+    }
+    
+    // Retornamos la función que representa el complemento
+    calcularComplemento
+  }
+
+  def inclusion(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = {
+    // Función auxiliar interna para aplicar recursión de cola
+    @scala.annotation.tailrec
+    def loop(i: Int): Boolean = {
+      if (i > 1000) true // Si hemos recorrido todos los elementos hasta 1000, s1 está incluido en s2
+      else if (pertenece(i, cd1) > pertenece(i, cd2)) false // Si algún elemento de s1 tiene mayor pertenencia que en s2, no está incluido
+      else loop(i + 1) // Continuamos con el siguiente elemento
+    }
+    loop(0) // Iniciamos la recursión desde 0
+  }
+  
 }
